f2c85fc5b50e70b6247d3d6f609c21e9b3c96060
Rangel Reale <rangelspam@gmail.com>
2020-11-19 10:30:42 -0300

    release 1.35

--- jsonpatchext/jsonpatchext.py
+++ jsonpatchext/jsonpatchext.py
@@ -57,7 +57,7 @@ except ImportError:
 
 # Will be parsed by setup.py to determine package metadata
 __author__ = 'Rangel Reale <rangelspam@gmail.com>'
-__version__ = '1.34'
+__version__ = '1.35'
 __website__ = 'https://github.com/RangelReale/python-json-patch-ext'
 __license__ = 'Modified BSD License'
 
